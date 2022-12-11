ca4f743571d18c73223880a52e66f3fc70f541b7
Valter Araujo <dear6730@mylaurier.ca>
2022-12-10 17:19:06 -0800

    Added 2 mockdata cards
    Total Stock value and Total Stock by Category
    Also, 2 examples.

--- imad-app/imad-web/src/main/webapp/imad-ui/webapp/controller/Dashboard.controller.js
+++ imad-app/imad-web/src/main/webapp/imad-ui/webapp/controller/Dashboard.controller.js
@@ -1,15 +1,18 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, JSONModel) {
         "use strict";
 
         return Controller.extend("ec.laurier.imad.controller.Dashboard", {
             onInit: function () {
-
+                var cardManifests = new JSONModel();
+                cardManifests.loadData("model/mockdata/cardsdata.json");
+                this.getView().setModel(cardManifests, "manifests");
             }
         });
     });
